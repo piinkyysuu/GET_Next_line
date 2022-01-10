c4b44f2763c067e99049a34de50ce4b8d3419002
Thu Le <thule@Thus-MacBook-Pro-2.local>
2022-01-10 11:16:32 +0200

    First commit

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thle <thle@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 16:16:28 by thule             #+#    #+#             */
+/*   Updated: 2022/01/04 14:31:42 by thle             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+# define FD_SIZE 4069
+
+int	get_next_line(const int fd, char **line);
+
+#endif
